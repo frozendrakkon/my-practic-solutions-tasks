e177d3452965161de4c659920dfbd2d4ef1f1ea4
FrozenDrakon <frozendrakon@sega.local>
2019-09-10 12:30:06 +0300

    education

--- task-data.js
+++ task-data.js
@@ -6,6 +6,16 @@ const data = [
     { name: "email", value: "test@email.com" },
     { name: "workRegion", value: "msk" }
 ];
-
-var some = { ...data };
+let some = {};
+data.forEach(({ name, value }) => {
+    some = { ...data[(value, name)] };
+});
 console.log(some);
+
+// const some  = {
+//     lastName: 'M',
+//     firstName: 'Vy',
+//     middleName: 'Gr',
+//     phone: 'tel:79178997773',
+//     email: 'mailto:test@email.com',
+//     workRegion: 'msk'
